17359500859100ee3eefceba5e44e0fae7628fda
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 15:57:08 +0900

    Create FourOne.java

---
+++ nanjava/chap5/FourOne.java
@@ -0,0 +1,12 @@
+public class FourOne {
+	public static void main(String[] args) {
+		int num=0, result=0;
+		
+		while(num<100) {
+			result += num;
+			num++;
+		}	
+		
+		System.out.println(result);
+	}
+}
