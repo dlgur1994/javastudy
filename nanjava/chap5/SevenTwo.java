3afbe6bc107a5b0a8af6ec2f00c42c67f0f54be2
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 15:59:39 +0900

    Create SevenTwo.java

---
+++ nanjava/chap5/SevenTwo.java
@@ -0,0 +1,10 @@
+public class SevenTwo {
+	public static void main(String[] args) {
+		for(int A=0 ; A<10 ; A++) {
+			for(int B=0 ; B<10 ; B++) {
+				if(A+B == 9)
+					System.out.println(A + " & " + B);
+			}
+		}
+	}
+}
