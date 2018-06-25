dc2f2124fe29481a754880daf3073ea07ffc4aeb
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 12:19:53 +0900

    Create OneFive.java

---
+++ nanjava/chap4/OneFive.java
@@ -0,0 +1,12 @@
+public class OneFive {
+	public static void main(String[] args) {
+		int A = ((25+5)+(36/4)-72)*5;
+		int B = ((25*5)+(36-4)+71)/4;
+		int C = (128/4)*2;
+		
+		if((A>B) && (B>C))
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
+}
