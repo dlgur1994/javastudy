0b93311d7981fb689aee8425c1f2ba152dfe82fa
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 09:15:56 +0900

    Create onethree.java

---
+++ nanjava/chap1/onethree.java
@@ -0,0 +1,9 @@
+public class OneOne3 {
+	public static void main(String[] args) {
+		System.out.println("12");
+		System.out.println(12);
+		System.out.println(3+9);
+		System.out.println("1" + "2");
+		System.out.println("1"+2);
+	}
+}
