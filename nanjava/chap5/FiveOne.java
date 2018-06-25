a626ca4dae0be5f5043e29761c058d75ea3673a1
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 15:58:14 +0900

    Create FiveOne.java

---
+++ nanjava/chap5/FiveOne.java
@@ -0,0 +1,10 @@
+public class FiveOne {
+	public static void main(String[] args) {
+		int result=1;
+		int num;
+		
+		for(num=1;num<10;num++)
+			result *= num;
+		System.out.println(result);
+	}
+}
