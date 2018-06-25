9835c5032d28442f82519b128d98a2f2902bf3ff
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-25 15:57:27 +0900

    Create FourTwo.java

---
+++ nanjava/chap5/FourTwo.java
@@ -0,0 +1,17 @@
+public class FourTwo {
+	public static void main(String[] args) {
+		int num=1;
+		
+		while(num<101) { 
+			System.out.println(num);
+			num++;
+		}
+		
+		num--;
+		
+		do {
+			System.out.println(num);
+			num--;
+		}while(num>0);
+	}
+}
