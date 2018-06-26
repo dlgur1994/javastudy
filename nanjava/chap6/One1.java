d251f57bbff50a17036c74042918ae8263cde60f
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-06-26 11:53:20 +0900

    Create One1.java

---
+++ nanjava/chap6/One1.java
@@ -0,0 +1,12 @@
+public class One1 {
+	public static void main(String[] args) {
+		pmmm(10, 4);
+	}
+	
+	public static void pmmm(int num1, int num2) {
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+	}
+}
