93999168ec4bef3e974d0b3b0132e56ca64b78d0
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-07-15 20:18:21 +0900

    Create Main.java

---
+++ nanjava/PhoneBook/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		PhoneInfo info1 = new PhoneInfo("Hyuk", "000", "000000");
+		PhoneInfo info2 = new PhoneInfo("Hy", "111");
+		
+		info1.showPhoneInfo();
+		info2.showPhoneInfo();
+	}
+}
