279f5d78295a6778d0287434a54351184482b202
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-07-19 23:11:26 +0900

    Create CheckExtend.java

---
+++ nanjava/chap14/CheckExtend.java
@@ -0,0 +1,36 @@
+public class CheckExtend {
+	public static void main(String[] args) {
+		HybridWaterCar car = new HybridWaterCar(10, 20, 30);
+		car.showCurrentGauge();
+	}
+}
+
+class Car {
+	int gasolineGauge;
+	
+	public Car(int gas) {
+		gasolineGauge = gas;
+	}
+}
+
+class HybridCar extends Car {
+	int electricGauge;
+	public HybridCar (int gas, int elec) {
+		super(gas);
+		electricGauge = elec;
+	}
+}
+
+class HybridWaterCar extends HybridCar {
+	int waterGauge;
+	public HybridWaterCar (int gas, int elec, int water) {
+		super(gas,elec);
+		waterGauge = water;
+	}
+	
+	public void showCurrentGauge() {
+		System.out.println("잔여 가솔린 : " + gasolineGauge);
+		System.out.println("잔여 전기량 : " + electricGauge);
+		System.out.println("잔여 워터량 : " + waterGauge);
+	}
+}
