b85fd2dffa75c7ee824ac0a92827a3fb37126b0c
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2018-12-21 20:09:08 +0900

    Create SubtractTwoNumbers.java

---
+++ baekjoon/SubtractTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SubtractTwoNumbers {
+	public static void main(String[] args) {
+		int num1, num2;
+		Scanner scan = new Scanner(System.in);
+	
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+	
+		System.out.println(num1-num2);
+	}
+}
