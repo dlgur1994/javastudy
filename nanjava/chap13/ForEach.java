ce9bdc58bc998f6658a4de9b164f3dc480e1c35f
dlgur1994 <38805855+dlgur1994@users.noreply.github.com>
2019-03-08 22:12:38 +0900

    Update ForEach.java

--- nanjava/chap13/ForEach.java
+++ nanjava/chap13/ForEach.java
@@ -2,38 +2,36 @@ import java.util.Scanner;
 
 public class ForEach {
 	public static void main(String[] args) {
-		int[] arr = new int[5];
-		int min,max;
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
 		
-		Scanner sc= new Scanner(System.in);
-		System.out.print("정수 입력: ");
-		
-		for(int i=0 ; i<5; i++) {
+		for(int i=0 ; i<10 ; i++) {
 			arr[i] = sc.nextInt();
 		}
 		
-		min = minValue(arr);
-		max = maxValue(arr);
-		
-		System.out.println("최소값 : " + min);
-		System.out.println("최대값 : " + max);
+		System.out.println("Max value is " + maxValue(arr));
+		System.out.println("Min value is " + minValue(arr));
 	}
 	
 	public static int minValue(int[] arr) {
 		int min = arr[0];
+		
 		for(int e : arr) {
-			if(e < min)
+			if(min>e)
 				min = e;
-		}		
+		}
+		
 		return min;
 	}
 	
 	public static int maxValue(int[] arr) {
 		int max = arr[0];
+		
 		for(int e : arr) {
-			if(e > max)
+			if(max<e)
 				max = e;
-		}		
+		}
+		
 		return max;
 	}
 }
